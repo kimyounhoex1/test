d6cb6f0fa8660785439bdb5ff8a82cd596fb8478
kimyounhoex1 <dordyd11@gmail.com>
2025-06-26 15:35:21 +0900

    chore(directory): directory 구조 변경

---
+++ soyeong/project/board/src/main/frontend/src/layout/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <header>
+      <hr />
+      푸터입니다..
+    </header>
+  );
+};
+
+export default Footer;
---
+++ soyeong/project/board/src/main/frontend/src/layout/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <a href="/">홈</a>
+      &nbsp;&nbsp;|&nbsp;&nbsp;
+      <a href="/board">게시판</a>
+      <hr />
+    </div>
+  );
+};
+
+export default Header;
